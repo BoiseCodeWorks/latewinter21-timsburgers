5a70f5065489bbb9c7f7451e704561a247d2ca51
TimDohmen <dohmentim@yahoo.com>
2021-04-07 13:26:28 -0600

    update line numbs

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace burgershack
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      //NOTE this is used to actually create the db connection - reference line 50-54
+      //NOTE this is used to actually create the db connection - reference lines 64-68
       services.AddScoped<IDbConnection>(x => CreateDbConnection());
 
       services.AddControllers();
