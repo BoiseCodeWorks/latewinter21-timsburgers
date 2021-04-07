9ba9e17f8c7b436dfdcc7a39c3ff46b2a54d9653
TimDohmen <dohmentim@yahoo.com>
2021-04-07 12:09:41 -0600

    init

---
+++ Setup.sql
@@ -0,0 +1,32 @@
+-- USE timsburgers;
+
+-- NOTE this is creating the table to put burgers in
+
+/* -- CREATE TABLE burgers
+-- (
+--   id INT AUTO_INCREMENT,
+--   name VARCHAR(255) NOT NULL UNIQUE,
+--   description VARCHAR(255),
+--   price DECIMAL(6 , 2) NOT NULL,
+
+--   PRIMARY KEY (id)
+-- ); */
+
+-- NOTE this is creating actual burger
+
+-- INSERT INTO burgers
+-- (name, description, price)
+-- VALUES
+-- ("Howdy Jonesy", "Brisket", 40)
+
+-- Get All of a collection
+-- SELECT * FROM burgers;
+
+-- Get a specific from a collection
+
+-- SELECT * FROM burgers WHERE description="Brisket" OR id=1;
+
+-- DELETE FROM burgers WHERE id=1;
+
+-- DELETE burgers;
+-- TRUNCATE burgers;
